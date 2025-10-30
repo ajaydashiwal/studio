3cc1394c1c8346479307f1f00982e726b252b0c6
Ajay Dashiwal <ajaydashiwal@gmail.com>
2025-10-30 10:23:13 +0000

    how to change the title of web app i.e. "Spreadsheet data viewer" to Upv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Spreadsheet Data Viewer',
+  title: 'Upvan Apartment RWA',
   description: 'View data from a Google Spreadsheet.',
 };
 
